5cd30c14d7ba112d14c3086d8b9974751f8886c1
tahakyo <ghazouani1988@email.com>
2022-04-27 22:51:11 +0200

    fail

--- js/script.js
+++ js/script.js
@@ -3,15 +3,31 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 // visualizzo 5 numeri random
+const numbersQuantity = 5 ;
+const rndmNumbersArray = generateRndmNumbers(numbersQuantity);
+document.getElementById('numbers').innerHTML = rndmNumbersArray;
+
 
 // li do un tempo di comparsa di 30 sec
+let timerContainer = document.getElementById('timer');
+let timeLeft = 10;
+timerContainer.innerHTML = timeLeft;
+
+const timeOut = setInterval(function(){
+    timeLeft--;
+    timerContainer.innerHTML = timeLeft;
+    if (timeLeft === 0) {
+        document.getElementById('numbers').classList.add("hidden");
+    }
+
+},1000);
+
+
 
 // poi chiedo all'utente di inserire i numeri che ha visto precedentemente tramite prompt
 
-// const rndmNumbers = getRndInteger(0, 100);
-const numbersQuantity = 5 ;
-const rndmNumbersArray = generateRndmNumbers(numbersQuantity);
-console.log(rndmNumbersArray);
+
+
 
 
 // UTILITY FUNCTIONS
